c5c72f300f261288b2257d0256ee59e7147e2d27
subham <subhammandal01@gmail.com>
2022-05-27 17:51:29 +0000

    updated script

--- ecs-terraform/infra_deploy.sh
+++ ecs-terraform/infra_deploy.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
-set -x 
+
+
+function create() {
+terraform init -input=false
+[ $? -eq 0 ] && terraform apply -auto-approve
+}
+
+function destroy() {
+terraform destroy -auto-approve
+}
+
+if [[ $DEPLOY == CREATE ]];then
+   cmd=create
+else
+   cmd=destroy
+fi
+
 export USERID=$(id -u)
 export PATH="$PATH:/usr/local/bin"
 export GROUPID=$(id -g)
 cd $(dirname $0)
+
 docker-compose -f compose.yaml \
-   run --rm -w "$WORKSPACE" -e DEPLOY=$DEPLOY \
-   --name terraform-${BUILD_NUMBER} --entrypoint "ecs-terraform/run.sh" terraform
+   run --rm -w "$WORKSPACE" \
+   --name terraform-${BUILD_NUMBER} $cmd
