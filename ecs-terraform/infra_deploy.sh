3823e84e60cb563052e61cf41f540a2e04640473
Subham Mandal <subhammandal01@gmail.com>
2022-05-27 23:33:56 +0530

    Update infra_deploy.sh

--- ecs-terraform/infra_deploy.sh
+++ ecs-terraform/infra_deploy.sh
@@ -9,6 +9,9 @@ if [[ $DEPLOY == "CREATE" ]];then
     docker-compose -f compose.yaml \
     run --rm -w "$WORKSPACE" \
     --name terraform-${BUILD_NUMBER} terraform -chdir=ecs-terraform/ init -input=false
+     docker-compose -f compose.yaml \
+    run --rm -w "$WORKSPACE" \
+    --name terraform-${BUILD_NUMBER} terraform -chdir=ecs-terraform/ apply -auto-approve
  else
     docker-compose -f compose.yaml \
     run --rm -w "$WORKSPACE" \
